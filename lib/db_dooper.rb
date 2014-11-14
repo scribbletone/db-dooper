b3423b003878283af62344cfe40714a68966963a
scribbletone <travis@scribbletone.com>
2014-11-14 14:57:46 -0800

    starting to map out the structure

--- lib/db_dooper.rb
+++ lib/db_dooper.rb
@@ -1,8 +1,16 @@
 class DbDooper
-  def self.hi(language)
-    translator = Translator.new(language)
-    translator.hi
+  def self.add_hooks
+    # adds hooks directory if it doesn't exist
+    FileUtils.mkdir_p '.git/hooks'
+
+    # if post-checkout file exists merge it in
+
+    # else duplicate file 
+
   end
 end
 
 require 'db_dooper/translator'
+require 'fileutils'
+
+
